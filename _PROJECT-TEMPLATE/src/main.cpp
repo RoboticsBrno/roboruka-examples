29d14d10d3f1c33a6bb2aa7b36ca3d0c23e490ee
Vojtech Bocek <vbocek@gmail.com>
2019-07-07 18:02:30 +0200

    Initial commit

---
+++ _PROJECT-TEMPLATE/src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+#include "roboruka.h"
+
+
+void setup() {
+    rkConfig cfg;
+    // Modify the cfg...
+    rkSetup(cfg);
+
+    // Write your setup
+}
+
+void loop() {
+
+}
---
+++ rbcontroller-android-app/src/main.cpp
@@ -0,0 +1,46 @@
+#include <Arduino.h>
+
+#include "roboruka.h"
+
+void handleMessage(const std::string& cmd, rbjson::Object *pkt) {
+    if(cmd == "joy") {
+        const rbjson::Array *data = pkt->getArray("data");
+        if(data->size() >= 1) {
+            const rbjson::Object *joy = data->getObject(0);
+            int x = joy->getInt("x");
+            int y = joy->getInt("y");
+
+            rkMotorsJoystick(x, y);
+        }
+    } else if(cmd == "arm") {
+        const double x = pkt->getDouble("x");
+        const double y = pkt->getDouble("y");
+        rkArmMoveTo(x, y);
+    } else if(cmd == "grab") {
+        rkArmSetGrabbing(!rkArmIsGrabbing());
+    }
+}
+
+void setup() {
+    rkConfig cfg;
+    cfg.owner = "FrantaFlinta";
+    cfg.name = "SuperRuka";
+
+    cfg.wifi_name = "RukoKraj";
+    cfg.wifi_password = "PlnoRukou";
+
+    cfg.rbcontroller_app_enable = true;
+    cfg.rbcontroller_message_callback = handleMessage;
+    rkSetup(cfg);
+
+    printf("%s's roboruka '%s' started!\n", cfg.owner, cfg.name);
+}
+
+static int gIter = 0;
+
+void loop() {
+    // Send text to the android application
+    rkControllerSendLog("Tick #%d, battery at %d%%, %dmv %d\n", gIter++, rkBatteryPercent(),
+        rkBatteryVoltageMv(), rkButtonIsPressed(1));
+    sleep(1);
+}
