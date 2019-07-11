8a007990fc649db2db17c03a666dbefcd3be9798
Jan Mrázek <email@honzamrazek.cz>
2019-07-11 15:33:13 +0200

    Add motor test

---
+++ motorTest/src/main.cpp
@@ -0,0 +1,24 @@
+#include <Arduino.h>
+#include "roboruka.h"
+
+
+void setup() {
+    Serial.begin(115200);
+    rkConfig cfg;
+    // Upravte nastavení, například:
+    cfg.motor_max_power_pct = 60; // limit výkonu motorů na 30%
+    rkSetup(cfg);
+
+    // Napište setup kód, provede se jednou po startu robota
+    rkLedBlue();
+}
+
+void loop() {
+    rkLedBlue(false);
+    if (rkButtonIsPressed(1, false)) {
+        rkMotorsSetPower(100, 100);
+    }
+    else {
+        rkMotorsSetPower(0, 0);
+    }
+}
