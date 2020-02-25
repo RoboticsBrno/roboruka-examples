94e6a24ad9258ce8f27c5265e02e5db12873ecc1
Vojtech Bocek <vbocek@gmail.com>
2020-02-25 20:14:40 +0100

    Enable bluetooth wifi config by default in rbcontroller app

--- rbcontroller-android-app/src/main.cpp
+++ rbcontroller-android-app/src/main.cpp
@@ -23,12 +23,19 @@ void handleMessage(const std::string& cmd, rbjson::Object *pkt) {
 
 void setup() {
     rkConfig cfg;
-    cfg.owner = "FrantaFlinta";
+    cfg.owner = "FrantaFlinta"; // Ujistěte se, že v aplikace RBcontrol máte nastavené stejné
     cfg.name = "SuperRuka";
 
-    // BUĎTO připojit na WiFi...
-    cfg.wifi_name = "RukoKraj";
-    cfg.wifi_password = "PlnoRukou";
+    // Ve výchozím lze WiFi na robotovi nastavit pomocí Android aplikace
+    // RBControl (verze 1.0 nebo novější) přes Bluetooth.
+    // Robot si toto nastavení pamatuje, a znovu ho použije při dalším zapnutí.
+
+    // Můžete WiFi i přímo nastavit zde v kódu, v takovém případě se nastavení
+    // nedá z aplikace změnit.
+
+    // Zde v kódu můžete BUĎTO připojit robota na WiFi...
+    //cfg.wifi_name = "RukoKraj";
+    //cfg.wifi_password = "PlnoRukou";
 
     // A NEBO vytvořit vlastní WiFi (odkomentovat další dva řádky)
     //cfg.wifi_default_ap = true;
